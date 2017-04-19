ddda33f67e1b153a9cb9a6a61cd309e0d15161ba
davidsluo <davidsluo@users.noreply.github.com>
2017-04-19 16:25:46 -0400

    fixed typos made in the sleepness night

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
             logging.info('Loading extension {0}'.format(ext))
             bot.load_extension(ext)
         except Exception as e:
-            logging.error('Failed to load extension {0]\n{1}:{2|'
+            logging.error('Failed to load extension {0}\n{1}:{2}'
                           .format(ext, type(e).__name__, e))
 
     bot.run(bot.config['discord']['token'])
