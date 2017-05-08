f9b0c54119549dbead6320e691e42fde6bf73440
davidsluo <davidsluo@users.noreply.github.com>
2017-05-08 00:55:46 -0400

    more maintainable slots command

--- cogs/casino.py
+++ cogs/casino.py
@@ -1,4 +1,6 @@
 import random
+from collections import namedtuple
+
 from discord.ext import commands
 from discord.ext.commands import Bot, Context
 
@@ -6,30 +8,43 @@ from cogs.economy import User
 
 
 class Casino:
-    slot_reel = [
-        '\N{GREEN APPLE}',
-        '\N{RED APPLE}',
-        '\N{PEAR}',
-        '\N{TANGERINE}',
-        '\N{LEMON}',
-        '\N{BANANA}',
-        '\N{WATERMELON}',
-        '\N{MELON}',
-        '\N{GRAPES}',
-        '\N{STRAWBERRY}',
-        '\N{PEACH}',
-        '\N{PINEAPPLE}',
-        '\N{TOMATO}',
-        '\N{HOT PEPPER}',
-        '\N{EAR OF MAIZE}',
-        '\N{CHERRIES}',
-        '\N{BELL}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER L}\N{REGIONAL INDICATOR SYMBOL LETTER V}',
-        '\N{DIGIT SEVEN}\N{COMBINING ENCLOSING KEYCAP}',
-    ]
+    Token = namedtuple('Token', ['emoji', 'weight', 'multiplier'])
 
-    weights = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 3.5, 3, 2.5]
-    multipliers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 5, 10, 50]
+    cherries = Token('\N{CHERRIES}', 4, 1)
+    bell = Token('\N{BELL}', 3.5, 5)
+    bar = Token('\N{REGIONAL INDICATOR SYMBOL LETTER L}\N{REGIONAL INDICATOR SYMBOL LETTER V}', 3, 10)
+    seven = Token('\N{DIGIT SEVEN}\N{COMBINING ENCLOSING KEYCAP}', 2.5, 50)
+
+    reel = (
+        Token('\N{GREEN APPLE}', 1, 0),
+        Token('\N{RED APPLE}', 1, 0),
+        Token('\N{PEAR}', 1, 0),
+        Token('\N{TANGERINE}', 1, 0),
+        Token('\N{LEMON}', 1, 0),
+        Token('\N{BANANA}', 1, 0),
+        Token('\N{WATERMELON}', 1, 0),
+        Token('\N{MELON}', 1, 0),
+        Token('\N{GRAPES}', 1, 0),
+        Token('\N{STRAWBERRY}', 1, 0),
+        Token('\N{PEACH}', 1, 0),
+        Token('\N{PINEAPPLE}', 1, 0),
+        Token('\N{TOMATO}', 1, 0),
+        Token('\N{HOT PEPPER}', 1, 0),
+        Token('\N{EAR OF MAIZE}', 1, 0),
+        cherries,
+        bell,
+        bar,
+        seven
+    )
+
+    weights = tuple(r.weight for r in reel)
+
+    reward_message = \
+        '**{0}** bet **${1:.2f}** and won **${2:.2f}**.\n'
+    reward_info = \
+        '{0} x{1}: **${2:.2f}**'
+    lost_message = \
+        '**{0}** bet **${1:.2f}** and lost all of it.'
 
     def __init__(self, bot: Bot):
         self.bot = bot
@@ -55,38 +70,45 @@ class Casino:
 
         better.balance -= amount
 
-        reel_1 = random.choices(self.slot_reel, weights=self.weights, k=3)
-        reel_2 = random.choices(self.slot_reel, weights=self.weights, k=3)
-        reel_3 = random.choices(self.slot_reel, weights=self.weights, k=3)
-        reel = [reel_1[1], reel_2[1], reel_3[1]]
-        counts = [reel.count(emoji) for emoji in self.slot_reel]
-        rewards = [amount * count * multiplier if count >= 2 or emoji == '\N{CHERRIES}' else 0
-                   for count, multiplier, emoji in zip(counts, self.multipliers, self.slot_reel)]
-        reward = sum(rewards)
+        reels = [
+            random.choices(self.reel, weights=self.weights, k=3),
+            random.choices(self.reel, weights=self.weights, k=3),
+            random.choices(self.reel, weights=self.weights, k=3)
+        ]
+        row = [reel[1] for reel in reels]
+
+        counts = {}
+        for token in row:
+            if token.multiplier > 0:
+                if token in counts:
+                    counts[token] += 1
+                else:
+                    counts[token] = 1
+
+        counts = {token: count for token, count in counts.items() if count >= 2 or token == self.cherries}
+
+        total = sum([token.multiplier * amount * count for token, count in counts.items()])
 
         slots_message = \
-            '__**     S  L  O  T  S     **__\n' \
-            '|| {0[0]} {1[0]} {2[0]} ||\n' \
-            '> {0[1]} {1[1]} {2[1]} <\n' \
-            '|| {0[2]} {1[2]} {2[2]} ||\n\n'
-        reward_message = \
-            '**{0}** bet **${1:.2f}** and won **${2:.2f}**.\n'
-        reward_info = \
-            '{0} x{1}: **${2:.2f}**'
-        lost_message = \
-            '**{0}** bet **${1:.2f}** and lost all of it.'
+            '__**   S   L   O   T   S   **__\n' \
+            '|| {0[0][0][0]} {0[1][0][0]} {0[2][0][0]} ||\n' \
+            '> {0[0][1][0]} {0[1][1][0]} {0[2][1][0]} <\n' \
+            '|| {0[0][2][0]} {0[1][2][0]} {0[2][2][0]} ||\n\n'
 
-        message = slots_message.format(reel_1, reel_2, reel_3)
-        if reward == 0:
-            message += lost_message.format(ctx.message.author, amount)
+        message = slots_message.format(reels)
+        if total == 0:
+            message += self.lost_message.format(ctx.message.author, amount)
         else:
-            message += reward_message.format(ctx.message.author, amount, reward)
-            message += '\n'.join([reward_info.format(emoji, count, count * amount * multiplier)
-                                  for emoji, count, multiplier in zip(self.slot_reel, counts, self.multipliers)
-                                  if (emoji == '\N{CHERRIES}' and count >= 1) or count >= 2])
+            message += self.reward_message.format(ctx.message.author, amount, total)
+
+            message += '\n'.join(
+                [self.reward_info.format(token.emoji, count, token.multiplier * count * amount)
+                 for token, count in counts.items()]
+            )
+
         await self.bot.say(message)
 
-        better.balance += reward
+        better.balance += total
         better.save()
 
 
